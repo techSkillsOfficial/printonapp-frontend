1622251baf638a4e421a14e249ed0572549a173b
Alamgeer <alamgeer.official.id@gmail.com>
2024-02-24 21:05:07 +0530

    add dashboard cards

--- src/component/Dashboard/Admindashboard.jsx
+++ src/component/Dashboard/Admindashboard.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import { FiCreditCard, FiMail,FiBook, FiUser,FiBarChart, FiUsers } from "react-icons/fi";
+import DashboardCard from "./DashboardCard";
 
-function Admindashboard() {
+const HoverDevCards = () => {
   return (
-    <div>Admindashboard</div>
-  )
-}
+    <div className="p-4 min-h-screen">
+      <p className="text-xl font-semibold mb-2">DashBoard</p>
+      <div className="grid gap-4 grid-cols-2 lg:grid-cols-4">
+        <DashboardCard
+          title="Account"
+          subtitle="Manage profile"
+          href="#"
+          Icon={FiUser}
+        />
+        <DashboardCard title="Order's" subtitle="Check your order status" href="#" Icon={FiBook} />
+        {/* <DashboardCard title="Team" subtitle="Manage team" href="#" Icon={FiUsers} />
+        <DashboardCard
+          title="Billing"
+          subtitle="Manage cards"
+          href="#"
+          Icon={FiCreditCard}
+        /> */}
+        
+      </div>
+    </div>
+  );
+};
 
-export default Admindashboard
+
+
+export default HoverDevCards;
